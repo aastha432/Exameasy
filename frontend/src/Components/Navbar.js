8dce2e8f2842d31d4ac43b48f3a089381c1c4aa6
Aastha Sarkar <aasthasarkar13gul@gmail.com>
2022-10-09 23:06:10 +0530

    Navbar styling completed

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,7 +7,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import HomeIcon from '@material-ui/icons/Home';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from '../firebase';
 import { logout } from '../firebase';
@@ -20,6 +21,7 @@ const useStyles = makeStyles((theme) => ({
   },
   menuButton: {
     marginRight: theme.spacing(2),
+    color : "#99ccff"
   },
   title: {
     flexGrow: 1,
@@ -29,18 +31,27 @@ const useStyles = makeStyles((theme) => ({
 const Navbar = () => {
 
     const [user, loading, error] = useAuthState(auth);
+    const navigate = useNavigate();
     const classes = useStyles();
 
   return (
       <AppBar className={classes.root}>
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-           <Link to="/"><HomeIcon /></Link>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu"
+           onClick={()=> navigate("/")}>
+            <HomeIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
           </Typography>
-          {!user ? <Button color="inherit"><Link to="/signin">Sign in</Link></Button> : null}
-          {!user ? <Button color="inherit"><Link to="/signup">Sign up</Link></Button> : null}
+         
+          {!user ? <Button type="submit" color="inherit"
+            onClick={()=> navigate("/signup")}>
+              Sign up
+          </Button> : null}
+          {!user ? <Button type="submit" color="inherit"
+            onClick={()=> navigate("/signin")}>
+              Sign in
+          </Button> : null}
           {user ? <Button type="submit" color="inherit"
             onClick={logout}>
               Sign out
