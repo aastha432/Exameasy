63b588065ad880820fa90d4f8af7b18a50fe3c30
Aastha Sarkar <aasthasarkar13gul@gmail.com>
2022-10-09 22:59:25 +0530

    Exam page styling completed

--- frontend/src/Components/Question.js
+++ frontend/src/Components/Question.js
@@ -3,24 +3,27 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
-import React from 'react';
+import React, {useState} from 'react';
 
 
 
 
-const Question = () => {
+const Question = ({question, option1, option2, option3, option4, correct}) => {
+
+    const [correctOption, setCorrectOption] = useState(correct);
+
     return (
       <FormControl>
-        <FormLabel id="demo-radio-buttons-group-label">Your sample question goes here.</FormLabel>
+        <FormLabel id="demo-radio-buttons-group-label">{question}</FormLabel>
         <RadioGroup
           aria-labelledby="demo-radio-buttons-group-label"
           defaultValue="female"
           name="radio-buttons-group"
         >
-          <FormControlLabel value="1" control={<Radio />} label="Option 1" />
-          <FormControlLabel value="2" control={<Radio />} label="Option 2" />
-          <FormControlLabel value="3" control={<Radio />} label="Option 3" />
-          <FormControlLabel value="4" control={<Radio />} label="Option 4" />
+          <FormControlLabel value="1" control={<Radio />} label={option1} />
+          <FormControlLabel value="2" control={<Radio />} label={option2} />
+          <FormControlLabel value="3" control={<Radio />} label={option3} />
+          <FormControlLabel value="4" control={<Radio />} label={option4} />
         </RadioGroup>
       </FormControl>
     );
--- frontend/src/Pages/Exam.js
+++ frontend/src/Pages/Exam.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
 import Typography from '@material-ui/core/Typography';
@@ -12,23 +12,46 @@ import Question from '../Components/Question';
 
 
 const useStyles = makeStyles((theme) => ({
-    actions: {
-      [theme.breakpoints.up('sm')]: {
-        display: 'flex'
-      }
-    },
-    primaryAction: {
-      width: '100%',
+    
+    submitButton: {
+      width: '30%',
       marginTop: theme.spacing(2),
+      backgroundColor : "#40bf80"
     },
     contentBox : {
-      marginTop : 100
+      marginTop : 30
     }
   }));
 
 const Exam = () => {
     const classes = useStyles();
     const navigate = useNavigate();
+    const [questions, setQuestions] = useState([
+      {
+        "question" : "A company is planning to set up a solution on the Azure platform. The solution has the following main key requirement. Provide a managed service that could be used to manage and scale container-based applications. Which of the following would be best suited for this requirement?",
+        "option1" : "When they have a hybrid solution",
+        "option2" : "When all of their servers are in the private cloud",
+        "option3" : "When all of their servers are in the public cloud",
+        "option4" : "When all of their servers are in the public or private cloud",
+        "correct" : "option3"
+      },
+      {
+        "question" : "When can an organization decommission its private cloud infrastructure hosted in its data center?",
+        "option1" : "Azure Event Grid",
+        "option2" : "Azure DevOps",
+        "option3" : "Azure Kubernetes",
+        "option4" : "Azure Dev Test Labs",
+        "correct" : "option3"
+      },
+      {
+        "question" : "Which of the following service is a cloud-based network security service that helps to protect the resources that are stored in an Azure Virtual Network?",
+        "option1" : "Azure Sentinel",
+        "option2" : "Azure Key Vault",
+        "option3" : "Azure Dedicated Hosts",
+        "option4" : "Azure Firewall",
+        "correct" : "option4"
+      }
+    ]); // should be populated by DB later
 
     const submitHandler = () => {
       navigate("/result");
@@ -36,9 +59,13 @@ const Exam = () => {
     return(
         <div>
             <Navbar/>
-            <Container maxWidth="xs" className={classes.contentBox}>
-                <Typography variant="h5" component="h3" align="center" gutterBottom={true}>Exam page</Typography>
-                <Grid item xs={12}>
+            <Container className={classes.contentBox}>
+
+              <Grid container spacing={2}>
+                <Grid item xs={11}>
+                  <Typography variant="h5" component="h3" align="center" gutterBottom={true}>Microsoft Certified : Azure Data Fundamentals</Typography>
+                </Grid>
+                <Grid item xs={1}>
                     <Webcam
                         audio={true}
                         height={100}
@@ -48,24 +75,34 @@ const Exam = () => {
                         videoConstraints={{width : 500, height: 500,facingMode: "user"}}
                     />
                 </Grid>
+              </Grid>
+                
+              <Grid container spacing={4}>
+                {
+                  questions.map((question)=>
+                  <Grid item xs={12}>
+                    <Question question={question.question}
+                    option1={question.option1}
+                    option2={question.option2}
+                    option3={question.option3}
+                    option4={question.option4}
+                    correct={question.correct}
+                    />
+                  </Grid>
+                  )
+                }
+              </Grid>
 
-              <Grid item xs={12}>
-                < Question/>
+              <Grid container spacing={5}>
+                <Grid item xs={12}>
+                  <Typography align='center'>
+                    <Button type="submit" variant="contained" size="large" className={classes.submitButton}
+                      onClick = {()=> submitHandler()}>
+                          Submit
+                      </Button>
+                  </Typography>
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                < Question/>
-              </Grid>
-              <Grid item xs={12}>
-                < Question/>
-              </Grid>
-              <Grid item xs={12}>
-                < Question/>
-              </Grid>
-
-              <Button type="submit" variant="contained" color="primary" size="large" className={classes.primaryAction}
-               onClick = {()=> submitHandler()}>
-                  Submit
-                </Button>
             </Container> 
             
         </div>
